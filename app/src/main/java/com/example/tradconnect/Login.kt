7f42b2c8bf50f0686fc44cf61c18b24dd82f0126
Katerin <fkaterin163@gmail.com>
2026-09-05 02:40:55 -0600

    Generacion de Splash Screen y actualizacion de login

--- app/src/main/java/com/example/tradconnect/Login.kt
+++ app/src/main/java/com/example/tradconnect/Login.kt
@@ -1,20 +1,44 @@
 package com.example.tradconnect
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.tradconnect.ui.MainActivity
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*Permite ver primero el splash screen y  luego el login*/
+
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val correo = findViewById<EditText>(R.id.edtxt_correo)
+        val contrasenia = findViewById<EditText>(R.id.edtxt_contrasena)
+        val acceder = findViewById<Button>(R.id.btn_acceder)
+
+        acceder.setOnClickListener{
+            val mail= correo.text.toString()
+            val password= contrasenia.text.toString()
+
+            if (mail == "Arlen23@gmail.com" && password =="262608"){
+                val ventana = Intent(
+                    this, MainActivity::class.java
+                )
+                startActivity(ventana)
+            }else{
+                Toast.makeText(this,"Usuario o contraseña incorrecto", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 }
